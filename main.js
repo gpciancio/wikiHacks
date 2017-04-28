18d4429ce03a30f49cc57eb9206261f4c1974ea6
gpciancio <gopalciancio@gmail.com>
2017-04-28 12:55:46 -0600

    added js with ajax

---
+++ main.js
@@ -0,0 +1,19 @@
+$("#search").on("click", function() {
+  var searchTerm = $("#searchTerm").val();
+var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+$.ajax({
+  url: url,
+  type: 'GET',
+  contentType: "application/json; charset=utf-8",
+  async: false,
+      dataType: "json",
+      // plop data
+      success: function(data, status, jqXHR) {
+        //console.log(data);
+        $("#output").html();
+        for(var i=0;i<data[1].length;i++){
+          $("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+        }
+
+      }
+})
